e72a73cc85e58e6fb79a71f0057d39e83b72a167
Natilyon21 <105010511+Natilyon21@users.noreply.github.com>
2023-05-13 10:23:28 -0700

    Create amenity.py

---
+++ models /amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+""" 
+Contains the class Amenity
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Representation of Amenity """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
