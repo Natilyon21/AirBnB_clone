d7340d88bbfa4fe6b35e190d9d17c668cbcc8e17
Natilyon21 <105010511+Natilyon21@users.noreply.github.com>
2023-05-13 10:17:40 -0700

    Create review.py

---
+++ models /review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+""" 
+Contains class Review
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """features of the Review """
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
