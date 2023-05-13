57b2636c15c3ab24245226f4eeb4094371d2014f
Natilyon21 <105010511+Natilyon21@users.noreply.github.com>
2023-05-13 10:13:45 -0700

    Create user.py

---
+++ models /user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+""" 
+contains the class User
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """feature of a user """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
